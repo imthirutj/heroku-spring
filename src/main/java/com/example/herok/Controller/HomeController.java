7e68a9fb68cda00af3312e508f1e7be3ff4bf98e
imthirutj <imthirutj@gmail.com>
2022-04-30 12:23:53 +0530

    better

---
+++ src/main/java/com/example/herok/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.herok.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String me() {
+		return "hello";
+	}
+	
+	
+}
